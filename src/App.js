9d0ed3e457305d5b8eee9810377ef9bdf12397a4
Erik Hanchett <ehhanche@amazon.com>
2022-07-22 11:41:51 -0700

    Start aws-amplify branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Storage, Auth } from "aws-amplify";
+import { useState, useEffect } from "react";
+import "@aws-amplify/ui-react/styles.css";
+import "./App.css";
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  async function upload() {}
+  async function grabFile() {}
+
+  async function list() {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Upload</button>
     </div>
   );
 }
